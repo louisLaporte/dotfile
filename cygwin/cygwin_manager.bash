c86ebec3f0346fa5dede864acd5f90f980b32e74
louisLaporte <louis.laporte12@gmail.com>
2016-03-10 16:44:39 -0500

    add cygwin manager and package
    Signed-off-by: louisLaporte <louis.laporte12@gmail.com>

---
+++ cygwin/cygwin_manager.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+
+##install apt-cyg
+MANAGER="apt-cyg"
+BIN_PATH="/usr/local/bin"
+
+
+install_manager(){
+if [ ! -e "$BIN_PATH/$MANAGER" ]
+then
+    wget raw.github.com/transcode-open/apt-cyg/master/apt-cyg
+    chmod +x $MANAGER
+    mv $MANAGER $BIN_PATH
+fi
+}
+
+install_packages(){
+    ./jq_cygwin_package.bash
+}
+set -x
+install_packages
+##or from setup
+#setup-x86.exe -q -P [package name]
