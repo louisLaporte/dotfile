d075542c6840c8b542f1d49d7b9e8431025d1ee1
louisLaporte <louis.laporte12@gmail.com>
2016-03-11 19:15:30 -0500

    add remove download from source, download binary
    Signed-off-by: louisLaporte <louis.laporte12@gmail.com>

--- install_jq.bash
+++ install_jq.bash
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
-URL="https://github.com/stedolan/jq.git"
-JQ=$(basename $URL)
 BIN_PATH="/usr/local/bin"
 
+rm ${BIN_PATH}/jq
 
-
-echo $JQ | cut -d . -f1
-git clone $URL
 
 if [ ! -e "$BIN_PATH/$JQ" ]
 then
-    cd $(echo $JQ | cut -d . -f1)
-    autoreconf -i
-    ./configure 
-    make && sudo make install
-    cp $JQ $BIN_PATH
-    cd ..
-    rm -rf ./$JQ
+    case $(uname -o| tr '[:upper:]' '[:lower:]') in
+        cygwin)
+            _jqUrlCygwin="https://github.com/stedolan/jq/releases/download/jq-1.5/jq-win64.exe "
+            _jqCygwin=$(basename ${_jqUrlCygwin} | cut -d "-" -f1)
+            wget $_jqUrlCygwin -O ${BIN_PATH}/${_jqCygwin} &>/dev/null
+            chmod +x ${BIN_PATH}/${_jqCygwin}
+            ls -l ${BIN_PATH}
+            which ${_jqCygwin}
+            ;;
+        *)
+            echo "aa"
+            ;;
+    esac
 fi
