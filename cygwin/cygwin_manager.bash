4d01b145899f61f3573f07b312686d5dea33646d
louisLaporte <louis.laporte12@gmail.com>
2016-03-15 16:26:37 -0400

    add some feature for powershell
    Signed-off-by: louisLaporte <louis.laporte12@gmail.com>

--- cygwin/cygwin_manager.bash
+++ cygwin/cygwin_manager.bash
@@ -1,25 +1,45 @@
 #!/usr/bin/env bash
-
-
+#???????????????????
+#
+#TODO: change FILE_PATH this is very ugly way to do that
+#
+#??????????????????
 
 ##install apt-cyg
-MANAGER="apt-cyg"
+MANAGER="raw.github.com/transcode-open/apt-cyg/master/apt-cyg"
 BIN_PATH="/usr/local/bin"
 
 
+# this is done like that because the script is sourcing in a parent folder (..)
+FILE_PATH="$(dirname ${BASH_SOURCE[0]})/cygwin_packages.json" 
+
+
+echo $(readlink -f $0)
+
+
 install_manager() {
-if [ ! -e "$BIN_PATH/$MANAGER" ]
-then
-    wget raw.github.com/transcode-open/apt-cyg/master/apt-cyg
-    chmod +x $MANAGER
-    mv $MANAGER $BIN_PATH
-fi
+    if [ ! -e "$BIN_PATH/$MANAGER" ]
+    then
+        wget $MANAGER -O ${BIN_PATH}/$(basename $MANAGER) &>/dev/null
+        chmod +x ${BIN_PATH}/$(basename $MANAGER) 
+    fi
+
 }
 
 install_packages() {
-    ./jq_cygwin_package.bash
+    printf "+- install basic packages\n"
+    for i in $(cat $FILE_PATH | jq '.[] | .basic | .[]'   | tr -d '"' | tr -d "\r")
+    do
+        # or setup-x86.exe -q -P [package name]
+        apt-cyg install $i 
+    done
+    printf "+- install extra packages\n"
+    for i in $(cat $FILE_PATH | jq '.[] | .extra | .[]'   | tr -d '"' | tr -d "\r")
+    do
+        # or setup-x86.exe -q -P [package name]
+        apt-cyg install $i 
+    done
 }
-set -x
-install_packages
-##or from setup
-#setup-x86.exe -q -P [package name]
+
+
+
